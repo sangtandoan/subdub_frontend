42cce615088371093ff98ea5e4fe448d2c4f5640
sangtandoan <sangvaminh11497@gmail.com>
2025-03-28 23:17:16 +0700

    refactor: change to data mode of react router v7

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import "./App.css";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Home from "./pages/Home";
-import { Routes, Route } from "react-router";
+import { Outlet } from "react-router";
 
 function App() {
     return (
         <>
-            <Routes>
-                <Route index element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
+            <Outlet />
         </>
     );
 }
---
+++ src/app/routes.jsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from "react-router";
+import App from "../App.jsx";
+import Home from "../pages/Home.jsx";
+import Login from "../pages/Login.jsx";
+import Register from "../pages/Register.jsx";
+import { redirect } from "react-router";
+
+async function homeLoader() {
+    console.log("home loader");
+    return redirect("/login");
+}
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: App,
+        children: [
+            { index: true, Component: Home, loader: homeLoader },
+            { path: "/login", Component: Login },
+            { path: "/register", Component: Register },
+        ],
+    },
+]);
+
+export default router;
