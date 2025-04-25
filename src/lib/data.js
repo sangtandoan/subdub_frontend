8fabf0e3f6edad65bead4aa1e28945a46d39568f
sangtandoan <sangvaminh11497@gmail.com>
2025-04-25 16:09:21 +0700

    fix: cors errors maybe without credentials

--- src/lib/data.js
+++ src/lib/data.js
@@ -13,13 +13,13 @@ async function getData(pagination, is_cancelled) {
     const url = `${import.meta.env.VITE_API_URL}/subscriptions/
 ?limit=${pagination.pageSize}&offset=${pagination.pageIndex * pagination.pageSize}&is_cancelled=${is_cancelled}`;
 
-    console.log(url);
     const response = await fetch(url, {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${localStorage.getItem("access_token")}`,
         },
+        credentials: "include",
     });
 
     const json = await response.json();
